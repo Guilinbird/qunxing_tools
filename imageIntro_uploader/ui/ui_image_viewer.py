835aed56486feb9a483a9bb76334333c42eb9ebf
ma138 <ma138078@163.com>
2020-04-13 23:07:45 +0800

    框架搭建

--- imageIntro_uploader/ui/ui_image_viewer.py
+++ imageIntro_uploader/ui/ui_image_viewer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-'''
+"""
 @Author  :   xiyu-mxx
 @Contact :   ma138078@163.com
 @File    :   ui_image_viewer.py
 @Time    :   2020/4/5 3:57
 @Desc    :
-'''
+"""
 import os
 import sys
 from PyQt5.QtCore import Qt, QSize, pyqtSignal
@@ -24,94 +24,97 @@ class ImageConverter(QMainWindow):
         super(ImageConverter, self).__init__(parent)
         self.cwd = os.getcwd()
         self.root = ''
-        self._initMenu()
-        self._initUI()
-        self._initPos()
+        self.open_img_list = {}  # key: abs_path; value: 1/0 current display
+        self._init_menu()
+        self._init_ui()
 
-    def _initMenu(self):
+    def _init_menu(self):
         self.menu = self.menuBar()
         self.fileMenu = self.menu.addMenu('文件')
         self.editMenu = self.menu.addMenu('编辑')
         self.viewMenu = self.menu.addMenu('视图')
         self.toolMenu = self.menu.addMenu('工具')
 
-        # self.fileMenu.addAction('导入图片', self.slotImportImg, Qt.CTRL+Qt.Key_I)
-        self.fileMenu.addAction('导入图片', self.slotImportImg, 'Ctrl+i')
-        self.fileMenu.addAction('导出图片', self.slotOutputImg, 'Ctrl+o')
+        self.fileMenu.addAction('导入图片', self.slot_import_img, 'Ctrl+o')
+        self.fileMenu.addAction('导出图片', self.slot_output_img, 'Ctrl+s')
         self.fileMenu.addAction('退出工具', qApp.quit, 'Ctrl+q')
 
-    def _initUI(self):
-        vlayMain = QVBoxLayout()
-        vlayMain.setContentsMargins(0, 0, 0, 0)
+    def _init_ui(self):
+        vlay_main = QVBoxLayout()
+        vlay_main.setSpacing(0)
+        vlay_main.setContentsMargins(0, 0, 0, 0)
         self.splitter = QSplitter(Qt.Horizontal)
         self.splitter.setMinimumSize(1200, 800)
 
-        self._initTab()
-        self._initDisplay()
-        self._initAttr()
-        vlayMain.addWidget(self.wgTab)
-        vlayMain.addWidget(self.splitter)
+        self._init_tab()
+        self._init_display()
+        self._init_attr()
+        vlay_main.addWidget(self.wg_tab)
+        vlay_main.addWidget(self.splitter)
         self.setCentralWidget(QWidget(self))
-        self.centralWidget().setLayout(vlayMain)
+        self.centralWidget().setLayout(vlay_main)
+        self.setStyleSheet("background: white")
 
-    def _initTab(self):
-        self.wgTab = QWidget()
-        self.wgTab.setFixedHeight(30)
-        # self.wgTab.setStyleSheet("background: #99FF99")
-        hlayTab = QHBoxLayout(self.wgTab)
-        hlayTab.addStretch(1)
-        hlayTab.setContentsMargins(0, 0, 0, 0)
+    def _init_tab(self):
+        self.wg_tab = QWidget()
+        self.wg_tab.setFixedHeight(30)
+        self.wg_tab.setStyleSheet("background: #F3F3F3")
+        hlay_tab = QHBoxLayout(self.wg_tab)
+        hlay_tab.addStretch(1)
+        hlay_tab.setContentsMargins(0, 0, 0, 0)
 
-    def _initPos(self):
-        uiFrame = self.frameGeometry()
-        centerPos = QDesktopWidget().availableGeometry().center()
-        uiFrame.moveCenter(centerPos)
-        self.move(100, 100)
-
-    def slotImportImg(self):
+    def slot_import_img(self):
         if self.root != '':
             self.cwd = self.root
-        path, type = QFileDialog.getOpenFileName(self, '选择图片', self.cwd, '*.jpg;*.jpeg;*.png;*.bmp')
-        self.root = os.path.split(path)[0]
-        if os.path.exists(path):
-            self.slotOpenImage(path)
-            self.slotAddTab(os.path.basename(path))
+        file_path, file_type = QFileDialog.getOpenFileName(self, '选择图片', self.cwd, '*.jpg;*.jpeg;*.png;*.bmp;')
+        self.root = os.path.split(file_path)[0]
+        if os.path.exists(file_path) and file_path not in self.open_img_list:
+            if len(self.open_img_list) != 0:
+                self.update_open_img()
+            self.slot_open_image(file_path)
+            self.slot_add_tab(os.path.basename(file_path), file_path)
+            self.open_img_list[file_path] = 1
 
-    def slotOutputImg(self):
+    def update_open_img(self):
+        not_display_img = [path for path, is_visible in self.open_img_list.items() if is_visible == 1][0]
+        self.open_img_list[not_display_img] = 0
+
+    def slot_output_img(self):
         print('导出图片')
 
-    def _initDisplay(self):
-        self.wgDisplay = QWidget(self.splitter)
-        vlayDisplay = QHBoxLayout()
-        vlayDisplay.setContentsMargins(0, 0, 0, 0)
-        self._initToolBar()
-        vlayDisplay.addWidget(self.wgToolBar)
+    def _init_display(self):
+        self.wg_display = QWidget(self.splitter)
+        vlay_display = QHBoxLayout()
+        vlay_display.setSpacing(0)
+        vlay_display.setContentsMargins(0, 0, 0, 0)
+        self._init_tool_bar()
+        vlay_display.addWidget(self.wg_tool_bar)
 
-        self.labelShower = QLabel()
-        self.labelShower.setMinimumSize(600, 600)
-        self.labelShower.setStyleSheet("background: #3366CC")
-        vlayDisplay.addWidget(self.labelShower)
-        self.wgDisplay.setLayout(vlayDisplay)
+        self.label_shower = QLabel()
+        self.label_shower.setMinimumSize(600, 600)
+        self.label_shower.setStyleSheet("background: #3366CC")
+        vlay_display.addWidget(self.label_shower)
+        self.wg_display.setLayout(vlay_display)
 
-    def _initToolBar(self):
-        self.wgToolBar = QWidget()
-        self.wgToolBar.setFixedWidth(20)
-        self.wgToolBar.setStyleSheet("background: #FF6633")
-        vlayTool = QVBoxLayout(self.wgToolBar)
-        vlayTool.setContentsMargins(0, 0, 0, 0)
+    def _init_tool_bar(self):
+        self.wg_tool_bar = QWidget()
+        self.wg_tool_bar.setFixedWidth(30)
+        self.wg_tool_bar.setStyleSheet("background: #FF6633")
+        vlay_tool = QVBoxLayout(self.wg_tool_bar)
+        vlay_tool.setContentsMargins(0, 0, 0, 0)
 
-    def _initAttr(self):
-        rightGroupBox = QGroupBox(self.splitter)
-        rightGroupBox.setMinimumWidth(300)
-        rightvlay = QVBoxLayout()
-        rightGroupBox.setLayout(rightvlay)
-        rightvlay.addWidget(QGroupBox('基本尺寸'))
-        rightvlay.addWidget(QGroupBox('详细尺寸'))
+    def _init_attr(self):
+        right_group_box = QGroupBox(self.splitter)
+        right_group_box.setMinimumWidth(300)
+        right_vlay = QVBoxLayout()
+        right_group_box.setLayout(right_vlay)
+        right_vlay.addWidget(QGroupBox('基本尺寸'))
+        right_vlay.addWidget(QGroupBox('详细尺寸'))
 
-    def slotOpenImage(self, imgPath):
-        img = QImage(imgPath)
+    def slot_open_image(self, img_path):
+        img = QImage(img_path)
         img_w, img_h = img.width(), img.height()
-        painter_w, painter_h = self.labelShower.size().width(), self.labelShower.size().height()
+        painter_w, painter_h = self.label_shower.size().width(), self.label_shower.size().height()
 
         w_scale = h_scale = 1
         if img_w > painter_w:
@@ -120,63 +123,64 @@ class ImageConverter(QMainWindow):
             h_scale = painter_h / img_h
         scale = min(w_scale, h_scale)
 
-        scaleImg = QPixmap.fromImage(img.scaled(QSize(int(img_w * scale), int(img_h * scale)), Qt.IgnoreAspectRatio))
-        self.labelShower.setPixmap(scaleImg)
+        scaleImg = QPixmap.fromImage(img.scaled(QSize(int(img_w * scale), int(img_h * scale))))
+        self.label_shower.setPixmap(scaleImg)
 
-    def slotAddTab(self, imgName):
-        addLabel = TabLabel(imgName)
-        self.wgTab.layout().insertWidget(0, addLabel)
+    def slot_add_tab(self, img_name, abs_path):
+        add_label = TabLabel(img_name, abs_path)
+        add_label.closedSignal.connect(self.slot_del_tab)
+        self.wg_tab.layout().insertWidget(0, add_label)
+
+    def slot_del_tab(self, abs_path):
+        if abs_path in self.open_img_list.keys():
+            self.findChild(QWidget, abs_path).deleteLater()
+            if self.open_img_list[abs_path] == 1:
+                self.label_shower.clear()
 
 
-class TabLabel(QLabel):
+class TabLabel(QWidget):
     """
-    custom tab label: fixed size, with close button, emit closed signal
+    custom tab label: fixed size, with close button
     """
-    closedSignal = pyqtSignal(int)
+    closedSignal = pyqtSignal(str)
 
-    def __init__(self, name):
-        super(TabLabel, self).__init__()
-        self.tabName = name
-        self._initUI()
+    def __init__(self, name, abs_path, parent=None):
+        super(TabLabel, self).__init__(parent)
+        self.tab_name = name.split('.')[0]
+        self.abs_path = abs_path
+        self.setObjectName(abs_path)
+        self.resize(QSize(35, 30))
+        self.setStyleSheet("background: #66CCFF; border: None; font:11pt 'Arial';")
+        self._init_ui()
 
-    def _initUI(self):
-        hlayLabel = QHBoxLayout()
-        labelContent = QLabel(self.tabName)
-        labelContent.setFixedSize(45, 25)
-        labelContent.setStyleSheet("background: #66CCFF; border-style: solid; font:12pt 'Arial';")
-        hlayLabel.addWidget(labelContent)
-        hlayLabel.addStretch(1)
-        butClose = QPushButton()
-        butClose.clicked.connect(self.slotEmitCloseSignal)
-        butClose.setStyleSheet("border-image: url(../../res/image/close.jpg)")
-        hlayLabel.addWidget(butClose)
-        self.setLayout(hlayLabel)
+    def _init_ui(self):
+        hlay_label = QHBoxLayout(self)
+        hlay_label.setSpacing(10)
+        hlay_label.setContentsMargins(0, 0, 0, 0)
 
-    def slotEmitCloseSignal(self):
-        self.closedSignal.emit()
+        label_content = QLabel(self.tab_name)
+        but_close = QPushButton()
+        but_close.setFixedSize(QSize(30, 30))
+        but_close.clicked.connect(self.slot_close_tab)
+        but_close.setStyleSheet("border: None; border-image: url(../../res/image/close.png)")
+
+        hlay_label.addSpacing(10)
+        hlay_label.addWidget(label_content)
+        hlay_label.addWidget(but_close)
+
+    def slot_close_tab(self):
+        self.closedSignal.emit(self.abs_path)
 
 
-class OpenImageCard(QLabel):
-    """
-    open image label
-    """
-
-    def __init__(self, imagePath):
-        super(OpenImageCard, self).__init__()
-        self.imagePath = imagePath
-        self.checkPath()
-        self.setAttr()
-
-    def checkPath(self):
-        if os.path.exists(self.imagePath) and os.path.splitext(self.imagePath)[1] in ['.jpg', '.png', '.bmp']:
-            return
-
-    def setAttr(self):
-        self.setObjectName(self.imagePath)
+test_path = r'D:\Guilinbird\qunxing_tools\test_data'
+def get_data():
+    file_list = os.listdir(test_path)
+    return file_list[0], os.path.join(test_path, file_list[0])
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = ImageConverter()
+    # name, abs_path = get_data()
     win.show()
     sys.exit(app.exec_())
